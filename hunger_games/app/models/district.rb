83c286cb8e70476c944b6125138a8daef807b7d8
daviskoh <koh.davis.0@gmail.com>
2013-11-25 18:52:57 -0500

    districts have citizens

--- hunger_games/app/models/district.rb
+++ hunger_games/app/models/district.rb
@@ -1,3 +1,13 @@
 class District < ActiveRecord::Base
-  has_many :citizen
+  validates :number, presence: true, uniqueness: true
+  validate :between_one_and_twelve
+  has_many :citizens
+
+  private
+
+  def between_one_and_twelve
+    return unless number.present?
+
+    errors.add(:number, "Needs to be between 1 and 12 inclusive") if self.number < 1 || self.number > 12
+  end
 end
--- hunger_games/spec/models/district_spec.rb
+++ hunger_games/spec/models/district_spec.rb
@@ -1,13 +1,28 @@
 require 'spec_helper'
 
-#TODO has citizens, must be between 1 and 12 inclusive
-
 describe District do 
-  let(:district) { District.new(number: 1) }
+  let(:district) { District.new(number: 1) } 
+  let(:citizen) { Citizen.new(type: "Tribute", name: "Bob", age: 12, sex: "male", alive: true, district_id: 1) }
 
   describe '::new' do 
-    it 'has a number between 1 and 12' do 
+    it 'has a number' do 
+      district.number = nil
+      expect(district).to have(1).errors_on(:number)
+    end
 
+    it 'has a number between 1 and 12' do 
+      district.number = 13
+      expect(district).to have(1).errors_on(:number)
+    end
+
+    it 'should have a unique number' do 
+      should validate_uniqueness_of(:number)
+    end
+
+    it 'can have citizens' do 
+      citizen.save
+      district.citizens << citizen
+      expect(district.citizens).to include(citizen)
     end
   end
 end
