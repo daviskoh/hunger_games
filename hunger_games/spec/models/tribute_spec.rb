2cd50386ca73a27a64aacfd5cb0aff7669a7fd95
daviskoh <koh.davis.0@gmail.com>
2013-11-26 20:13:10 -0500

    clean up test code

--- hunger_games/spec/models/tribute_spec.rb
+++ hunger_games/spec/models/tribute_spec.rb
@@ -1,20 +1,12 @@
 require 'spec_helper'
 
 describe Tribute do 
-  # before do
-  #   $t = self
-  # end
-
   let(:tribute) { Tribute.new }
   let(:game) { Game.new(number: 1) }
   let(:district) { District.new(number: 1) }
   let(:citizen) { Citizen.new(name: "Steve", age: 12, sex: "male", alive: true, district_id: district.id) }
 
   describe '::new' do 
-    # it 'has either volunteered or not' do 
-    #   expect(tribute).to have(1).errors_on(:volunteer)
-    # end
-
     it 'is part of a game' do 
       expect(tribute).to have(1).errors_on(:game_id)
 
